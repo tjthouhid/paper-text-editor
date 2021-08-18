fab4de23f2cf486ea51de7cf9ca0f298b63540b7
Tj Thouhid <tjthouhid@gmail.com>
2021-08-19 03:12:40 +0600

    first commit

---
+++ paper-text-editor.php
@@ -0,0 +1,10 @@
+<?php
+/* 
+Plugin Name: Paper text Editor 
+Plugin URI: https://github.com/tjthouhid/paper-text-editor/
+Description: This Plugin paper text editor
+Version: 1.0.1 
+Author: Tj Thouhid 
+Author URI: https://www.tjthouhid.me/
+License: GPLv2 or later 
+*/
