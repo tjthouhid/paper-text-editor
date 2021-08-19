05f93c562a9edf5becc1c1feae744012f93a4da1
Tj Thouhid <tjthouhid@gmail.com>
2021-08-19 22:19:02 +0600

    added setting page

--- paper-text-editor.php
+++ paper-text-editor.php
@@ -15,37 +15,225 @@ function paper_text_editor_view($atts = array())
 
     global $wpdb;
     extract(shortcode_atts(array(
-      	'buttontext' => 'Find',
-	    'labeltext' => 'Zip Code',
-	    'phtext' => 'Type Your Zip Code Here...',
+      	'buttontext1' => 'Send',
+      	'buttontext2' => 'Send',
+	
    ), $atts));
-    if(isset($_POST['check_zip'])){
-    	$zip = $_POST['zipcode'];
-    	
-    	$table_name = $wpdb->prefix . 'zip_list';
-	    $result = $wpdb->get_results( "SELECT * FROM $table_name WHERE zip='$zip'" );
-	    if(count($result)>0){
-	        $success_url = get_option('zip_success_url');
-	        ?>
-	        <script type="text/javascript">
-				var url = "<?php echo $success_url;?>";
-				window.location.href = url;
-			</script>
-			<?php 
-	    }else{
-	        $failure_url = get_option('zip_failure_url');
-	        ?>
-	        <script type="text/javascript">
-				var url = "<?php echo $failure_url;?>";
-				window.location.href = url;
-			</script>
-			<?php 
-	    }
-    	exit;
-    }
+    $tx_compay_email = get_option('tx_compay_email');
+	$tx_image_id = get_option('tx_image_id');
+	$tx_longtext = get_option('tx_longtext');
     ob_start();
     include 'templates/view.php';
     $content = ob_get_clean();
     return $content;
 }
 add_shortcode('paper-text-editor', 'paper_text_editor_view');
+
+add_action("wp_ajax_send_user_text_email", "send_user_text_email");
+add_action("wp_ajax_nopriv_send_user_text_email", "send_user_text_email");
+function send_user_text_email() {
+	$name = $_REQUEST["name"];
+	$email = $_REQUEST["email"];
+	$phone = $_REQUEST["phone"];
+	$text_value = $_REQUEST["text_value"];
+	$tx_compay_email = get_option('tx_company_email');
+
+	$message = "Name : ".$name. "\r\n";
+	$message .= "Email : ".$email. "\r\n";
+	if($phone!=""){
+		$message .= "Phone : ".$phone. "\r\n";
+	}
+	$message .= "Text : ".$text_value. "\r\n";
+
+	$to = $tx_compay_email;
+	$subject = get_option('tx_company_email_subject');
+	$headers = 'From: '. $email . "\r\n" .
+    'Reply-To: ' . $email . "\r\n";
+    $sent = wp_mail($to, $subject, strip_tags($message), $headers);
+      if($sent) {
+      	$result['type'] = "success";
+      	$result['msg'] = get_option('tx_success_msg');;
+      }//message sent!
+      else  {
+      	$result['type'] = "error";
+      	$result['msg'] = "Mail Sent Failed";
+      }//message wasn't sent
+
+      $result = json_encode($result);
+      echo $result;
+      die();
+}
+
+add_action( 'admin_menu', 'paper_text_setting' );
+
+function paper_text_setting() {
+	$page_title='Paper Text';
+	$menu_title='Paper Text';
+	$capability=1;
+	$menu_slug='paper_text_setting';
+	$function='paper_text_setting_template';
+	$icon_url='';
+	$position=68.9;
+	
+	add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
+}
+function paper_text_setting_template(){
+	global $wpdb;
+	if(isset($_POST['update'])){
+        update_option('tx_company_email_subject',$_POST['tx_company_email_subject']);
+        update_option('tx_company_email',$_POST['tx_company_email']);
+        update_option('tx_image_id',$_POST['tx_image_id']);
+        update_option('tx_longtext',$_POST['tx_longtext']);
+        update_option('tx_success_msg',$_POST['tx_success_msg']);
+    }
+	$tx_company_email_subject = get_option('tx_company_email_subject');
+	if($tx_company_email_subject ==""){
+		$tx_company_email_subject = "Sent Mail From Paper Text";
+	}
+	$tx_company_email = get_option('tx_company_email');
+	if($tx_company_email ==""){
+		$tx_company_email = "tjthouhid@gmail.com";
+	}
+	$tx_image_id = get_option('tx_image_id');
+	$tx_longtext = get_option('tx_longtext');
+	$tx_success_msg = get_option('tx_success_msg');
+	if($tx_success_msg ==""){
+		$tx_success_msg = "Message sent successfully!";
+	}
+	
+	?>
+	<style type="text/css">
+		.ctable{
+			margin: 60px 0px;
+		}
+		.ctable tr{}
+		.ctable tr th{
+			padding: 20px 0px;
+    		text-align: left;
+		}
+		.ctable tr td{
+			padding: 20px 20px;
+		}
+		.ctable input{
+			width: 300px;
+		}
+		.ctable img{
+			width: 120px;
+		}
+		.ctable a{
+			display: block;
+		}
+		.ctable textarea{
+			width: 300px;
+		    height: 160px;
+		    resize: none;
+		}
+		.ctable button{
+			background-color: #4CAF50; /* Green */
+			border: none;
+			color: white;
+			padding: 15px 32px;
+			text-align: center;
+			text-decoration: none;
+			display: inline-block;
+			font-size: 16px;
+			margin-top: 20px;
+		}
+		.ctable button:hover{
+			background-color: #64d668; /* Green */
+			cursor: pointer;
+		}
+	</style>
+	<script type="text/javascript">
+		jQuery(function($){
+
+			// on upload button click
+			$('body').on( 'click', '.misha-upl', function(e){
+
+				e.preventDefault();
+
+				var button = $(this),
+				custom_uploader = wp.media({
+					title: 'Insert image',
+					library : {
+						// uploadedTo : wp.media.view.settings.post.id, // attach to the current post?
+						type : 'image'
+					},
+					button: {
+						text: 'Use this image' // button label text
+					},
+					multiple: false
+				}).on('select', function() { // it also has "open" and "close" events
+					var attachment = custom_uploader.state().get('selection').first().toJSON();
+					button.html('<img src="' + attachment.url + '">').next().val(attachment.id).next().show();
+				}).open();
+			
+			});
+
+			// on remove button click
+			$('body').on('click', '.misha-rmv', function(e){
+
+				e.preventDefault();
+
+				var button = $(this);
+				button.prev().val(''); // emptying the hidden field
+				button.hide().prev().prev().html('Upload image');
+			});
+
+		});
+	</script>
+	<h1>Paper Text Setting</h1>
+	<form action="" method="POST">
+	<table class="ctable">
+		<tr>
+			<th>Email Subject: </th>
+			<td><input type="text" name="tx_company_email_subject" value="<?php echo $tx_company_email_subject;?>"></td>
+		</tr>
+		<tr>
+			<th>Company Email : </th>
+			<td><input type="email" name="tx_company_email" value="<?php echo $tx_company_email;?>"></td>
+		</tr>
+		<tr>
+			<th>Stamp Image :</th>
+			<td>
+				<?php if( $image = wp_get_attachment_image_src( $tx_image_id ) ) {
+
+					echo '<a href="#" class="misha-upl"><img src="' . $image[0] . '" /></a>
+					      <input type="hidden" name="tx_image_id" value="' . $image_id . '">
+					      <a href="#" class="misha-rmv">Remove image</a>';
+
+				} else {
+
+					echo '<a href="#" class="misha-upl">Upload image</a>
+						  <input type="hidden" name="tx_image_id" value="">
+						  <a href="#" class="misha-rmv" style="display:none">Remove image</a>';
+
+				} ?>
+			</td>
+		</tr>
+		<tr>
+			<th>Long Text :</th>
+			<td>
+				<textarea name="tx_longtext"><?php echo $tx_longtext;?></textarea>
+			</td>
+		</tr>
+		<tr>
+			<th>Success Message: </th>
+			<td><input type="text" name="tx_success_msg" value="<?php echo $tx_success_msg;?>"></td>
+		</tr>
+		<tr>
+			<td>
+			</td>
+			<td>
+				<button type="submit" name="update">Update</button>
+			</td>
+		</tr>
+	</table>
+	</form>
+	<?php
+}
+
+function load_media_files() {
+    wp_enqueue_media();
+}
+add_action( 'admin_enqueue_scripts', 'load_media_files' );
--- templates/view.php
+++ templates/view.php
@@ -4,7 +4,110 @@
 	<form action="" method="post">
 		<div class="paper">
 		    <div class="holes"></div>
-		    <textarea placeholder="Kurt Vonnegut was a guy. He wrote some stuff."></textarea>
+		    <textarea placeholder="Please Type Here" id="text_value"></textarea>
 		</div>
+		<button id="openForm" type="button" class="t-button b-blue">Send</button>
 	</form>
+	<!-- The Modal -->
+	<div id="sendText" class="t-modal">
+		<!-- Modal content -->
+	  	<div class="t-modal-content">
+	    	<span class="close">&times;</span>
+	    	<div class="modal-form">
+	    		<?php if( $image = wp_get_attachment_image_src( $tx_image_id ) ) { ?>
+	    			<img src="<?php echo $image[0];?>">
+	    		<?php }?>
+	    		<form action="" method="post">
+	    			<p><?php echo $tx_longtext;?></p>
+	    			<label>Name</label>
+	    			<input type="text" name="name" id="name">
+	    			<label>Email</label>
+	    			<input type="email" name="email" id="email">
+	    			<label>Phone <span>(Optional)</span></label>
+	    			<input type="text" name="phone" id="phone">
+	    			<button id="send" type="button" class="t-button b-blue">Send</button>
+
+	    		</form>
+	    	</div>
+	  	</div>
+	</div>
 </div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script type="text/javascript">
+	var ajaxUrl = "<?php echo admin_url( 'admin-ajax.php' );?>";
+jQuery(function($){
+	$("#send").on("click",function(e){
+		e.preventDefault();
+		var text_value = $("#text_value");
+		var name = $("#name");
+		var email = $("#email");
+		var phone = $("#phone");
+		if(name.val() == ""){
+			alert("Please Enter Name");
+			return false;
+		}
+		if(email.val() == ""){
+			alert("Please Enter Email");
+			return false;
+		}
+		if(!validateEmail(email.val())){
+			alert("Please Enter Valid Email");
+			return false;
+		}
+
+		$.ajax({
+			type : "post",
+		    dataType : "json",
+		    url : ajaxUrl,
+		    data : {
+		    	action: "send_user_text_email", 
+		    	name : name.val(), 
+		    	email : email.val(), 
+		    	phone : phone.val(),
+		    	text_value : text_value.val()
+		    },
+		    success: function(response) {
+		    	if(response.type == "success") {
+		    		alert(response.msg)
+		        }else{
+		        	alert(response.msg);
+		        }
+		        location.reload();
+		    }
+		});   
+
+	});
+
+});
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+	// Get the modal
+var modal = document.getElementById("sendText");
+
+// Get the button that opens the modal
+var btn = document.getElementById("openForm");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
